197604c1c68b3fc3871e0eb05ea9d88de663c095
Stinger18 <ethanhowlett01@gmail.com>
2024-10-23 08:00:04 -0600

    Chat history is now saved in a database and will remain between chat sessions. The documents still need work.

--- src/chatOllama.py
+++ src/chatOllama.py
@@ -10,6 +10,10 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import PromptTemplate
+
 from database import add_document, add_query, fetch_documents, fetch_queries, clear_table
 
 import subprocess
@@ -50,6 +54,10 @@ import requests
 
 
 ollama_llm = Ollama(model='llama3.2')
+
+if 'vectors' not in st.session_state:
+    st.session_state['vectors'] = None # Initialize the vector store
+
 def ask_ollama(user_input):
     return ollama_llm.invoke(user_input)
 
@@ -76,6 +84,7 @@ with st.sidebar.expander("Settings"):
         st.success("Chat history cleared!")
     if st.button("Clear documents"):
         clear_table("documents")
+        st.session_state['vectors'] = None # Reset the vectors when documents are cleared
         st.success("Documents cleared!")
 
 # Sidebar for user inputs
@@ -85,31 +94,51 @@ uploaded_files = st.sidebar.file_uploader("",type=["pdf", "txt"], accept_multipl
 
 # Process uploaded documents and create a vector store
 if uploaded_files:
-    all_texts = []
-    counter = 0
     for uploaded_file in uploaded_files:
-        all_texts.append(load_document(uploaded_file))
+        for file_name, _ in fetch_documents():
+            if uploaded_file.name == file_name:
+                break
+        else:
+            add_document(file_name=uploaded_file.name, content=load_document(uploaded_file))
+            st.sidebar.success("Documents uploaded and processed!")
+            st.session_state['vectors'] = None # Reset the vectors when new documents are uploaded
 
-        # Split the texts into manageable chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-        documents = text_splitter.split_text("\n".join(all_texts))
-        
-        add_document(file_name=uploaded_file.name, content=documents)
-        counter += 1
 
-    st.sidebar.success("Documents uploaded and processed!")
-    st.sidebar.write(counter)
+# Create embeddings for the documents
+documents = fetch_documents()
+if documents and st.session_state['vectors'] is None:
+    # Split the texts into manageable chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    split_doc = text_splitter.split_text("\n".join(text for _, text in documents))
 
-    if fetch_documents():
-        # Create embeddings for the documents
-        documents = fetch_documents()
-        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-        vector_store = FAISS.from_texts([chunk for doc in all_texts for chunk in doc], embeddings)
+    # st.write(split_doc)
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    st.session_state['vectors'] = FAISS.from_texts(split_doc, embeddings)
+    st.sidebar.success("Vectors created!")
+else:
+    st.session_state['vectors'] = None
+    
 
 if st.sidebar.button("show documents"):
     for file_name, content in fetch_documents():
-        st.sidebar.write(f"Filename: {file_name} - Content: {content[:100]}...")
+        st.sidebar.write(f"Filename: {file_name}")
     
+# Create the RAG chain
+def rag_chain(vectors, prompt, llm):
+    
+    retriever = vectors.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+    ragChain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return ragChain
+
+# Define the prompt template
+prompt = PromptTemplate(template="Answer the following question using given context and return with citations: {context}. Question: {question}", input_variables=["context", "question"])
+
 
 # Start chat
 if (fetch_queries() == []):
@@ -120,16 +149,22 @@ user_input = st.chat_input(placeholder='Message Ollama')
 if user_input:
     # Get response from Ollama
     # Create a context for the question
-    try:
-         # Perform a similarity search in the vector store
-        relevant_docs = vector_store.similarity_search(user_input)
-
-        context = "\n".join([doc.page_content for doc in relevant_docs])
-        response = ask_ollama(f"{context}\n\nQuestion: {user_input}\nAnswer:")
-    except Exception as e:
+    # Perform a similarity search in the vector store
+    if st.session_state['vectors'] is None:
         response = ask_ollama(f"{user_input}\nAnswer:")
-
-    add_query(user_input, response)
+    else:
+        # Load llama3.2 model into the rag chain
+        ragChain = rag_chain(vectors=st.session_state['vectors'], prompt=prompt, llm=ollama_llm)
+        similar = st.session_state['vectors'].similarity_search_with_score(user_input)
+        for doc in similar:
+            if doc[1] < 1.7:
+                response = ragChain.invoke(user_input)
+                add_query(user_input, f'FROM TEXT: \n {response}')
+                break
+            else:
+                response = ask_ollama(f"{user_input}\nAnswer:")
+                add_query(user_input, response)
+                break
 
 # Display chat history
 for query, response, timestamp in reversed(fetch_queries()):
